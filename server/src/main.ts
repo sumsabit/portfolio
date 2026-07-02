bc0aa39fb0fd599eca45bad7558a8a6e226d0577
sumsabit <sumeyasabit1@gmail.com>
2026-07-02 00:03:13 -0500

    Fix build script, update main.ts, messages module, tsconfig

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173', // <-- changed to 8080
+    origin: 'http://localhost:5173', 
     credentials: true,
   });
 
