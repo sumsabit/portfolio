cd7f1960adb770f6a82755d4cb5cb2e543fc7449
sumsabit <sumeyasabit1@gmail.com>
2026-07-02 09:23:12 -0500

    Fix Vite alias issue

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'dns': false, // Prevents Vite from trying to bundle it
-    },
-  },
   define: {
     global: 'window',
   },
