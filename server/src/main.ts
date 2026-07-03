89260277992f9c2669c28f29ac3d60bfb3e13226
sumsabit <sumeyasabit1@gmail.com>
2026-07-03 09:02:09 -0500

    Update CORS configuration to allow requests from any origin

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173', 
+    origin: '*', 
     credentials: true,
   });
 
